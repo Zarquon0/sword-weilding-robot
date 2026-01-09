b4aafd706401301a225a8e19fddb98c41cf954ca
William Stone <william_l_stone@brown.edu>
2026-01-09 12:24:37 -0500

    Resetting properly implemented

--- src/arm_commander.py
+++ src/arm_commander.py
@@ -2,10 +2,13 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float64MultiArray
 from duel_msgs.msg import DuelBotObservation
+from std_srvs.srv import Trigger
+from rclpy.task import Future   
 import numpy as np
 
 OBSERVATION_TOPIC = "arm_observations"
 COMMAND_TOPIC = "arm_command_targets"
+RESET_SERVICE = "duel_bot/reset"
 QUEUE_LENGTH = 10
 # FREQUENCY = 20 # In hertz
 
@@ -14,7 +17,12 @@ class ArmCommander(Node):
         super().__init__('arm_controller')
         self.subscription  = self.create_subscription(DuelBotObservation, OBSERVATION_TOPIC, self.observation_response, QUEUE_LENGTH)
         self._publisher = self.create_publisher(Float64MultiArray, COMMAND_TOPIC, QUEUE_LENGTH)
-        #self.timer = self.create_timer(1 // FREQUENCY, self.timer_callback) 
+        self.reset_client = self.create_client(Trigger, RESET_SERVICE)
+        self.pause_publishing = False
+        # Initial Reset
+        print("Waiting for reset service registration")
+        self.reset_client.wait_for_service()
+        self.training_reset()
 
     # NOTE: Not used currently for readability's sake
     def _flatten_message(self, msg: DuelBotObservation):
@@ -32,8 +40,28 @@ class ArmCommander(Node):
             else: # Atomic value
                 flat_list.append(float(value))
         return flat_list
+    
+    def training_reset(self):
+        print("Resetting")
+        self.pause_publishing = True
+        self.reset_client.wait_for_service()
+        reset_fut = self.reset_client.call_async(Trigger.Request())
+        reset_fut.add_done_callback(self.on_reset_complete)
+    
+    def on_reset_complete(self, fut: Future):
+        resp = fut.result()
+        if resp.success: self.pause_publishing = False
+        else: raise Exception("Reset failed (resp.success = false)")
 
     def observation_response(self, msg: DuelBotObservation):
+        # If node is in paused state, do nothing
+        if self.pause_publishing:
+            print("PUBLISHING PAUSED!")
+            return None
+        # If the cube was hit, reset everything and exit
+        if msg.hit_target:
+            self.training_reset()
+            return None
         # Combine all data into an array for training
         model_ipts = [
             msg.relative_target_position.x, msg.relative_target_position.y, msg.relative_target_position.z,
